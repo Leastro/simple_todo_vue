71db4c4e3a47fcfb1513dd6f42a0aed4432238b7
jani_cb <artti3623@gmail.com>
2025-01-26 15:31:06 +0900

    2025.01.26 첫 화면 틀 잡기 1

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
 
-const routes = [
+/*const routes = [
   {
     path: '/',
     name: 'home',
@@ -13,15 +13,22 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   }
   ,
   {
     path: '/Main',
     name: 'Main',
-    component: () => import(/* webpackChunkName: "about" */ '../views/MainView.vue')
+    component: () => import('../views/MainView.vue')
   }
 ]
+*/
+
+const routes = [{
+  path: '/',
+  name: 'main',
+  component: MainView
+}]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,57 @@
 <template>
-    <div class="Main">
-      <h1>여기는 메인 페이지 입니다.</h1>
+    <div class="main">
+      <h1 class="title">당신의 할 일 목록's - To_Do List</h1>
+    </div>
+
+    <div class="body">
+      <div class="button_head">
+        <button class="add">+등록</button>
+        <select name="statue" id="" class="pl">
+            <option value="0" selected>상태</option>
+            <option value="complete">완료</option>
+            <option value="ing">미완료</option>
+        </select>
+        <input type="text" class="manager" placeholder="담당자">
+        <button class="search">검색</button>
+      </div>
+      <div class="list_table">
+        <table>
+          <caption>todo리스트</caption>
+          <colgroup>
+              <col style="width:8%;">
+              <col style="width:auto;">
+              <col style="width:7%;">
+              <col style="width:7%;">
+              <col style="width:10%;">
+              <col style="width:10%;">
+              <col style="width:10%;">
+          </colgroup>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>내용</th>
+              <th>담당자</th>
+              <th>상태</th>
+              <th>기한</th>
+              <th>등록시간</th>
+              <th>수정/삭제</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>테스트</td>
+              <td>김길동</td>
+              <td>완료</td>
+              <td>2025-01-05</td>
+              <td>2025-01-02</td>
+              <td>
+                <img class="edit" alt="eidt">
+                <img class="remove" alt="remove">
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </template>
